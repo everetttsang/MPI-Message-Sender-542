488756a52c81a2a2f5193d82ab23988d6026f1e9
everettt <everettt@discovery.hpc.usc.edu>
2020-09-18 00:39:06 -0700

    Created 2 node system + print statements identifying each node.

--- p2/pt2.c
+++ p2/pt2.c
@@ -3,28 +3,37 @@
 
 int main(int argc, char** argv) {
   // Initialize the MPI environment. The two arguments to MPI Init are not
-  // currently used by MPI implementations, but are there in case future
-  // implementations might need the arguments.
-  MPI_Init(NULL, NULL);
+  //   // currently used by MPI implementations, but are there in case future
+  //     // implementations might need the arguments.
+        MPI_Init(NULL, NULL);
+  //
+  //         // Get the number of processes
+             int world_size;
+               MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  //
+  //               // Get the rank of the process
+                   int world_rank;
+                     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  //
+  //                     // Get the name of the processor
+                         char processor_name[MPI_MAX_PROCESSOR_NAME];
+                          int name_len;
+                             MPI_Get_processor_name(processor_name, &name_len);
+  //
+  //
+                               if(world_rank==0){
+                                   printf("Hello I'm processor %s with rank %d\n", processor_name, world_rank);
+} 
 
-  // Get the number of processes
-  int world_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+		if (world_rank ==1){
+			printf("Hello I'm processor %s with rank %d\n",processor_name, world_rank); 
+		}		                           
 
-  // Get the rank of the process
-  int world_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-  // Get the name of the processor
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int name_len;
-  MPI_Get_processor_name(processor_name, &name_len);
-
-
-  if(world_rank==0){
-    printf("Hello I'm processor %s with rank %d\n", processor_name, world_rank);
-  }
-
-  // Finalize the MPI environment. No more MPI calls can be made after this
-  MPI_Finalize();
-}
+		if(world_rank ==2){
+			printf("Hello I'm processor %s with rank %d\n", processor_name, world_rank);
+		}
+  //                                     
+  //                                       // Finalize the MPI environment. No more MPI calls can be made after this
+                                           MPI_Finalize();
+                                           }
+  //
