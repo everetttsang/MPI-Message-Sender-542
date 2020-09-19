788dd4b5c00787aa18e899df14f9025802c81e5e
aaryan <aaryan@discovery.hpc.usc.edu>
2020-09-19 13:55:21 -0700

    Completed Part 2

--- p2/pt2.c
+++ p2/pt2.c
@@ -2,90 +2,76 @@
 #include <stdio.h>
 #include <math.h>
 #include <sys/time.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-  struct timeval ts;
   // Initialize the MPI environment. The two arguments to MPI Init are not
-  //   // currently used by MPI implementations, but are there in case future
-  //     // implementations might need the arguments.
+  // currently used by MPI implementations, but are there in case future
+  // implementations might need the arguments.
   MPI_Init(NULL, NULL);
-  //
-  //         // Get the number of processes
+
+  // Get the number of processes
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-  //
+
   // Get the rank of the process
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-  //
+
   // Get the name of the processor
   char processor_name[MPI_MAX_PROCESSOR_NAME];
   int name_len;
   MPI_Get_processor_name(processor_name, &name_len);
-  //
-  //
+
+  struct timeval start;
+  struct timeval end;
+  int *data = malloc(134217728 * sizeof(int)); // Allocate 512 MB of ints to send
+  int maxSize = pow(2, 30);
+  int byteSize = sizeof(MPI_INT);
   int i;
-  short int number1=1;
-  printf("Size of number1: %d\n", sizeof(number1));
-  int number2;
+  int numElements;
+  long int rttSum;
+  double rttAvg;
 
+  if (world_rank == 0){
+    while(byteSize <= maxSize){
+      rttSum = 0;
+      numElements = byteSize / sizeof(MPI_INT);
 
-  if(world_rank==0){
-    //printf("Hello I'm processor %s with rank %d\n", processor_name, world_rank);
+      for(i = 0; i < 1000; i++){
+        gettimeofday(&start, NULL);
 
-    for (i=0; i<30; i++){
-      long int timesToSend = pow ( 2, i);
-      printf("Sending number size %d %ld times.\n", sizeof(number1), timesToSend);
-      int time1;
-      int time2;
-      long int rtt;
-      long long int dataToSend=0;
-      int j;
-      int k;
-      for (k=0; k< timesToSend; k++){
+        MPI_Send(data, numElements, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(data, numElements, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-
-        if(k==0){
-          gettimeofday(&ts, NULL);
-          time1=ts.tv_sec;
-        }
-        MPI_Send(&number1, sizeof(number1), MPI_INT, 1, 0, MPI_COMM_WORLD);
-        dataToSend+= sizeof(number1);
-        MPI_Recv(&number1, sizeof(number1), MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        if(k==timesToSend-1){
-          gettimeofday(&ts, NULL);
-          time2= ts.tv_sec;
-        }
+        gettimeofday(&end, NULL);
+              
+        rttSum += (end.tv_sec*1e6 + end.tv_usec) - (start.tv_sec*1e6 + start.tv_usec);
       }
+      
+      printf("RTT Sum: %d\n", rttSum);
+      rttAvg = (double) rttSum / 2000;
+      printf("Buffer Size: %d\n", byteSize);
+      printf("Number of Int Elements: %d\n", numElements);
+      printf("Average Round Trip Time: %f\n\n", rttAvg);
 
-      rtt = time2-time1;
-      printf("Data sent: %ld bytes. RTT: %ld\n", dataToSend, rtt);
-
+      byteSize = byteSize * 2;
     }
-
   }
 
-  if (world_rank ==1){
-    for (i=0; i<31; i++){
-      long int timesToSend = pow ( 2, i);
+  if (world_rank == 1){
+    while(byteSize <= maxSize){
+      numElements = byteSize / sizeof(MPI_INT);
 
-      int j;
-      int k;
-      for (k=0; k< timesToSend; k++){
-        MPI_Recv(&number2, sizeof(number2), MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        //printf("Received %d from node0\n",number2);
-        MPI_Send(&number2, sizeof(number2), MPI_INT, 0, 0, MPI_COMM_WORLD);
-
+      for(i = 0; i < 1000; i++){
+        MPI_Recv(data, numElements, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(data, numElements, MPI_INT, 0, 0, MPI_COMM_WORLD);
       }
 
-
-
+      byteSize = byteSize * 2;
     }
-
   }
-
 
   // Finalize the MPI environment. No more MPI calls can be made after this
   MPI_Finalize();
 }
-//
