f1c888b79eb8734520c1a96a145cb3086c39a06b
everetttsang <everettt@usc.edu>
2020-09-18 23:38:12 -0700

    introduce 1000 trials + average rtt

--- p2/pt2.c
+++ p2/pt2.c
@@ -29,27 +29,38 @@ int main(int argc, char** argv) {
     int number1=pow(2, i);
     printf("Size of number1: %d\n", sizeof(number1));
     int number2;
-    int time1;
-    int time2;
-    int rtt;
-    if(world_rank==0){
-      printf("Hello I'm processor %s with rank %d\n", processor_name, world_rank);
-      gettimeofday(&ts, NULL);
-      time1=ts.tv_usec;
-      MPI_Send(&number1, sizeof(number1), MPI_INT, 1, 0, MPI_COMM_WORLD);
-      MPI_Recv(&number1, sizeof(number1), MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      gettimeofday(&ts, NULL);
-      time2= ts.tv_usec;
-      rtt = time2-time1;
-      printf("Received %d from node1\nRTT: %d microseconds.\n",number1,rtt);
-    }
 
-    if (world_rank ==1){
+    int j;
+    int rttSum=0;
+    double rttAvg;
+    for (j=0; j<1000-; j++){
 
-      MPI_Recv(&number2, sizeof(number2), MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      printf("Received %d from node0\n",number2);
-      MPI_Send(&number2, sizeof(number2), MPI_INT, 0, 0, MPI_COMM_WORLD);
+      int time1;
+      int time2;
+      int rtt;
+      if(world_rank==0){
+        //printf("Hello I'm processor %s with rank %d\n", processor_name, world_rank);
+        gettimeofday(&ts, NULL);
+        time1=ts.tv_usec;
+        MPI_Send(&number1, sizeof(number1), MPI_INT, 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(&number1, sizeof(number1), MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        gettimeofday(&ts, NULL);
+        time2= ts.tv_usec;
+        rtt = time2-time1;
+        rttSum= rttSum + rtt;
+        //printf("Received %d from node1\tRTT: %d microseconds.\n",number1,rtt);
+      }
+
+      if (world_rank ==1){
+
+        MPI_Recv(&number2, sizeof(number2), MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //printf("Received %d from node0\n",number2);
+        MPI_Send(&number2, sizeof(number2), MPI_INT, 0, 0, MPI_COMM_WORLD);
+      }
     }
+    rttAvg = rttSum/ 1000.0;
+    printf("Sent %d 1000x. Average RTT = %d.\n", number1, avgRTT);
+
   }
 
   // Finalize the MPI environment. No more MPI calls can be made after this
