344eee2830b7b164148d53ef3a5d04b7c40f11cd
everetttsang <everettt@usc.edu>
2020-09-19 00:34:42 -0700

    transfer

--- p2/pt2.c
+++ p2/pt2.c
@@ -25,39 +25,48 @@ int main(int argc, char** argv) {
   //
   //
   int i;
-  for (i=0; i<31; i++){
-    int number1=pow(2, i);
-    printf("Size of number1: %d\n", sizeof(number1));
-    int number2;
+  int number1=1;
+  printf("Size of number1: %d\n", sizeof(number1));
+  int number2;
 
+  for (i=0; i<31; i++){
+    int timesToSend = pow ( 2, i);
     int j;
     int rttSum=0;
     double rttAvg;
     for (j=0; j<1000; j++){
+      int k;
+      int totalDataSent=0;
+      for (k=0; k< timesToSend; k++){
+        int time1;
+        int time2;
+        int rtt;
+        if(world_rank==0){
+          //printf("Hello I'm processor %s with rank %d\n", processor_name, world_rank);
+          gettimeofday(&ts, NULL);
+          time1=ts.tv_usec;
+          MPI_Send(&number1, sizeof(number1), MPI_INT, 1, 0, MPI_COMM_WORLD);
+          MPI_Recv(&number1, sizeof(number1), MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          gettimeofday(&ts, NULL);
+          time2= ts.tv_usec;
+          rtt = time2-time1;
+          rttSum= rttSum + rtt;
+          totalDataSent += sizeof(number1);
+          //printf("Received %d from node1\tRTT: %d microseconds.\n",number1,rtt);
+        }
 
-      int time1;
-      int time2;
-      int rtt;
-      if(world_rank==0){
-        //printf("Hello I'm processor %s with rank %d\n", processor_name, world_rank);
-        gettimeofday(&ts, NULL);
-        time1=ts.tv_usec;
-        MPI_Send(&number1, sizeof(number1), MPI_INT, 1, 0, MPI_COMM_WORLD);
-        MPI_Recv(&number1, sizeof(number1), MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        gettimeofday(&ts, NULL);
-        time2= ts.tv_usec;
-        rtt = time2-time1;
-        rttSum= rttSum + rtt;
-        //printf("Received %d from node1\tRTT: %d microseconds.\n",number1,rtt);
+        if (world_rank ==1){
+
+          MPI_Recv(&number2, sizeof(number2), MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          //printf("Received %d from node0\n",number2);
+          MPI_Send(&number2, sizeof(number2), MPI_INT, 0, 0, MPI_COMM_WORLD);
+        }
       }
-
-      if (world_rank ==1){
-
-        MPI_Recv(&number2, sizeof(number2), MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        //printf("Received %d from node0\n",number2);
-        MPI_Send(&number2, sizeof(number2), MPI_INT, 0, 0, MPI_COMM_WORLD);
+      if(j==0){
+        printf("Total data sent %d\n", totalDataSent);
       }
     }
+
     rttAvg = rttSum/ 1000.0;
     printf("Sent %d 1000x. Average RTT = %d.\n", number1, rttAvg);
 
